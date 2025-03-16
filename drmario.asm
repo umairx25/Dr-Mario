8fe8e704ea2d57ad48abf6cc4370768f4c21a867
umairx25 <umairarhambd@gmail.com>
2025-03-15 21:08:48 -0400

    Added names

--- drmario.asm
+++ drmario.asm
@@ -1,8 +1,8 @@
 ################# CSC258 Assembly Final Project ###################
 # This file contains our implementation of Dr Mario.
 #
-# Student 1: Name, Student Number
-# Student 2: Name, Student Number (if applicable)
+# Student 1: Umair Arham, 1010246565
+# Student 2: Sameer Shahed, 1010313876 
 #
 # We assert that the code submitted here is entirely our own 
 # creation, and will indicate otherwise when it is not.
